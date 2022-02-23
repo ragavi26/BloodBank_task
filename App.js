fc9e52c97775fc1c5c9d71e9a2d846e28480d185
ragavi26 <ragz2610@gmail.com>
2022-02-23 20:01:43 +0530

    react-hook-form

--- App.js
+++ App.js
@@ -1,175 +1,174 @@
-import {
-  ScrollView,
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Button,
-  Alert,
-} from 'react-native';
-import React, {useState} from 'react';
+import React, {Component} from 'react';
+import {TextInput, Text, Button, Alert, View, StyleSheet} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as yup from 'yup';
+import {Formik} from 'formik';
 
-export default function App() {
-  const [state, setState] = useState({
-    Name: '',
-    Age: '',
-    DOB: '',
-    blood_group: '',
-    address: '',
-    email: '',
-    ph: '',
-  });
-
-  const storeData = () => {
-    const email_regex =
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    const date_regex = /^\d{2}([./-])\d{2}\1\d{4}$/;
-    const blood_regex = /^(A|B|AB|O)[+-]$/i;
-    if (
-      email_regex.test(state.email) === true &&
-      state.ph.length === 10 &&
-      date_regex.test(state.DOB) === true &&
-      blood_regex.test(state.blood_group)
-    ) {
-      const jsonValue = JSON.stringify(state);
-      AsyncStorage.setItem('@storage_Key', jsonValue);
-    } else {
-      Alert.alert('Invalid Text');
-    }
-  };
-  //hi
-
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('@storage_Key');
-      if (value !== null) {
-        console.log("<====Fetching from Async Storage====>")
-        console.log(value);
-        // value previously stored
+export default class App extends Component {
+  render() {
+    const inputStyle = {
+      borderWidth: 1,
+      borderColor: '#4e4e4e',
+      padding: 12,
+      marginBottom: 5,
+    };
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('@storage_Key');
+        if (value !== null) {
+          console.log("<====Fetching from Async Storage====>")
+          console.log(value);
+          // value previously stored
+        }
+      } catch (e) {
+        // error reading value
       }
-    } catch (e) {
-      // error reading value
-    }
-  };
-  return (
-    <View style={styles.container}>
-      <Text style={styles.fullscreen}>BLOOD BANK🩸</Text>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Name"
-          placeholderTextColor="#003f5c"
-          value={state.Name}
-          type="text"
-          onChangeText={e => setState({...state, Name: e})}
-          name="Name"
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Age"
-          placeholderTextColor="#003f5c"
-          value={state.Age}
-          type="text"
-          keyboardType="numeric"
-          onChangeText={e => setState({...state, Age: e})}
-          name="Age"
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Date - dd-mm-yyyy"
-          placeholderTextColor="#003f5c"
-          type="text"
-          keyboardType="numeric"
-          onChangeText={e => setState({...state, DOB: e})}
-          name="DOB"
-          value={state.DOB}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Blood Group"
-          placeholderTextColor="#003f5c"
-          value={state.blood_group}
-          type="text"
-          onChangeText={e => setState({...state, blood_group: e})}
-          name="blood_group"
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Address"
-          placeholderTextColor="#003f5c"
-          type="text"
-          onChangeText={e => setState({...state, address: e})}
-          name="address"
-          value={state.address}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email"
-          placeholderTextColor="#003f5c"
-          value={state.email}
-          type="text"
-          onChangeText={e => setState({...state, email: e})}
-          name="email"
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Phone"
-          placeholderTextColor="#003f5c"
-          type="text"
-          keyboardType="numeric"
-          onChangeText={e => setState({...state, ph: e})}
-          name="ph"
-          value={state.ph}
-        />
-      </View>
-      <Button title="Submit" onPress={storeData} />
-      <Button title="Fetch Data" onPress={getData} />
-    </View>
-  );
+    };
+
+    return (
+      <Formik
+        initialValues={{
+          Name: '',
+          Age: '',
+          DOB: '',
+          blood_group: '',
+          address: '',
+          email: '',
+          ph: '',
+        }}
+        onSubmit={values => Alert.alert(JSON.stringify(values))}
+        validationSchema={yup.object().shape({
+          Name: yup.string().required('Please, provide your name!'),
+          Age: yup.number().positive().integer().required(),
+          blood_group: yup.string()
+          .required("Please enter the required field")
+          .matches(/^(A|B|AB|O)[+-]$/i, "Only alphabets are allowed for this field "),
+          address: yup.string().required('Please, provide your address!'),
+          email: yup.string().email().required('Email is required'),
+          ph: yup
+            .string()
+            .required('This field is Required')
+            .matches(
+              /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+              'Phone number is not valid',
+            ),
+        })}>
+        {({
+          values,
+          handleChange,
+          errors,
+          setFieldTouched,
+          touched,
+          isValid,
+          handleSubmit,
+        }) => (
+         
+         
+          <View style={styles.formContainer}>
+             <Text style={styles.fullscreen}>BLOOD BANK🩸</Text>
+           
+            <TextInput
+              value={values.Name}
+              style={inputStyle}
+              onChangeText={handleChange('Name')}
+              onBlur={() => setFieldTouched('Name')}
+              placeholder="Name"
+            />
+            {touched.Name && errors.Name && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.Name}
+              </Text>
+            )}
+            <TextInput
+              value={values.Age}
+              style={inputStyle}
+              onChangeText={handleChange('Age')}
+              onBlur={() => setFieldTouched('Age')}
+              placeholder="Age"
+            />
+            {touched.Age && errors.Age && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.Age}
+              </Text>
+            )}
+            <TextInput
+              value={values.blood_group}
+              style={inputStyle}
+              onChangeText={handleChange('blood_group')}
+              onBlur={() => setFieldTouched('blood_group')}
+              placeholder="blood-group"
+            />
+            {touched.blood_group && errors.blood_group && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.blood_group}
+              </Text>
+            )}
+           
+            <TextInput
+              value={values. address}
+              style={inputStyle}
+              onChangeText={handleChange('address')}
+              onBlur={() => setFieldTouched('address')}
+              placeholder="address"
+            />
+            {touched.address && errors.address && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.address}
+              </Text>
+            )}
+             
+            <TextInput
+              value={values.email}
+              style={inputStyle}
+              onChangeText={handleChange('email')}
+              onBlur={() => setFieldTouched('email')}
+              placeholder="E-mail"
+            />
+            {touched.email && errors.email && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.email}
+              </Text>
+            )}
+            <TextInput
+              value={values.ph}
+              style={inputStyle}
+              onChangeText={handleChange('ph')}
+              placeholder="Phone Number"
+              onBlur={() => setFieldTouched('ph')}
+              secureTextEntry={true}
+            />
+            {touched.ph && errors.ph && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.ph}
+              </Text>
+            )}
+            <Button
+              color="#3740FE"
+              title="Submit"
+             // disabled={!isValid}
+              onPress={handleSubmit}
+            />
+            <Button title="Fetch Data" onPress={getData} />
+          </View>
+          
+        )}
+      </Formik>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputView: {
-    backgroundColor: '#F3C892',
-    borderRadius: 30,
-    width: '70%',
-    height: 45,
-    marginBottom: 20,
 
-    alignItems: 'center',
-  },
-  TextInput: {
-    height: 50,
-    width: '100%',
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
+  formContainer: {
+    padding: 50,
   },
   fullscreen:{
     marginBottom:50,
-    marginTop:-100,
+    marginTop:50,
+    marginLeft:70,
     fontSize:22,
     fontWeight:'bold',
      color:'red'
     
   }
+  
 });
